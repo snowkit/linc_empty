d7a7ebff3d5509be50483dfa742a36f780ecd79a
underscorediscovery <sven@underscorediscovery.com>
2015-09-13 17:17:34 -0230

    initial commit

---
+++ linc/linc_empty.cpp
@@ -0,0 +1,11 @@
+#include "./linc_empty.h"
+
+#include <hxcpp.h>
+
+namespace linc {
+
+    namespace empty {
+
+    } //empty namespace
+
+} //linc
---
+++ linc/linc_empty.h
@@ -0,0 +1,16 @@
+#ifndef _LINC_EMPTY_H_
+#define _LINC_EMPTY_H_
+    
+// #include "../lib/____"
+
+#include <hxcpp.h>
+
+namespace linc {
+
+    namespace empty {
+
+    } //empty namespace
+
+} //linc
+
+#endif //_LINC_EMPTY_H_
